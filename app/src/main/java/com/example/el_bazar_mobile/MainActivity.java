3472f9533f71c69582363700080cf3f2165af03f
besrour walid <walidbesrour@gmail.com>
2020-11-26 18:13:20 +0100

    version 1

---
+++ app/src/main/java/com/example/el_bazar_mobile/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.el_bazar_mobile;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class MainActivity extends AppCompatActivity {
+    private ChipNavigationBar chipNavigationBar;
+    private Fragment fragment = null;
+
+
+    Context context ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
+     //  getSupportActionBar().setIcon(R.drawable.ic_baseline_menu_24);
+      // getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
+
+        context = MainActivity.this;
+
+
+
+
+        nav_bar();
+
+
+
+
+
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true ;
+    }
+
+    public void nav_bar(){
+        chipNavigationBar = findViewById(R.id.chipNavigation);
+
+        chipNavigationBar.setItemSelected(R.id.home, true);
+        //   getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                switch (i) {
+                    case R.id.home:
+                        //     fragment = new HomeFragment();
+                        break;
+                    case R.id.cart:
+//                        fragment = new CartFragment();
+                        break;
+                    case R.id.profile:
+//                        fragment = new ProfileFragment();
+                        break;
+                    case R.id.reglage:
+//                        fragment = new ProfileFragment();
+                        break;
+
+                }
+
+                if (fragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                }
+            }
+        });
+    }
+
+
+
+
+}
